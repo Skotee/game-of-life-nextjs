d439aebf94cee3597ffd8b8801ddd086caede269
Radoslaw Wojaczek <radoslaw_wojaczek@epam.com>
2023-10-23 02:20:19 +0200

    update next.config file

--- next.config.js
+++ next.config.js
@@ -1,14 +1,2 @@
 /** @type {import("next").NextConfig} */
-module.exports = () => {
-	const rewrites = () => {
-		return [
-			{
-				source: "/",
-				destination: "https://ai-proxy.epam-rail.com/:path*",
-			},
-		];
-	};
-	return {
-		rewrites,
-	};
-};
+module.exports = NextConfig;
